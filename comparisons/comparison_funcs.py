a17c642d88483177b3cb3d7f6d90d291633aa8ac
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2022-01-15 14:14:05 -0800

    Add example that solves factorial, not Fibonacci

--- comparisons/comparison_funcs.py
+++ comparisons/comparison_funcs.py
@@ -1,46 +1,21 @@
 import difflib
 import os
-import re
 from pathlib import Path
-from typing import Tuple, List
 
 import openai
 
+from comparisons.file_parser import file_path_to_code_string
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-docstring = re.compile(r"(\"\"\"|''')(?:(?=(\\?))\2(.|\n))*?\1")
-
-
-def strip_comments(text: List[str]) -> str:
-    out = ""
-    for line in text:
-        no_comments = line.split("#")[0]
-        out += no_comments
-    return out
-
-
-def open_and_preprocess_files(
-    starter_file: Path, submission_file: Path, solution_file: Path
-) -> Tuple[str, str, str]:
-    with open(starter_file) as file:
-        starter = strip_comments(file.readlines())
-    with open(submission_file) as file:
-        submission = strip_comments(file.readlines())
-    with open(solution_file) as file:
-        solution = strip_comments(file.readlines())
-
-    starter = re.sub(docstring, "", starter)
-    submission = re.sub(docstring, "", submission)
-    solution = re.sub(docstring, "", solution)
-
-    return starter, submission, solution
 
 
 def compare_submission_solution(
     starter_file: Path, submission_file: Path, solution_file: Path
 ):
-    starter, submission, solution = open_and_preprocess_files(
-        starter_file, submission_file, solution_file
+    starter, submission, solution = (
+        file_path_to_code_string(starter_file),
+        file_path_to_code_string(submission_file),
+        file_path_to_code_string(solution_file),
     )
 
     engine = openai.Engine("davinci-codex")
@@ -48,7 +23,7 @@ def compare_submission_solution(
     out = engine.search(
         search_model="davinci-codex", documents=[starter, solution], query=submission
     )
-    return out
+    return out["data"][0]["score"], out["data"][1]["score"]
 
 
 def compare_submission_solution_diff(
@@ -75,14 +50,8 @@ if __name__ == "__main__":
     starter = Path("examples/starter.py")
     submission_recur = Path("examples/submission_recur.py")
     submission_invalid = Path("examples/submission_invalid.py")
+    submission_wrong = Path("examples/submission_wrong_function.py")
     solution = Path("examples/solution.py")
-    print(
-        compare_submission_solution(
-            starter, submission_recur, solution
-        )
-    )
-    print(
-        compare_submission_solution(
-            starter, submission_invalid, solution
-        )
-    )
+
+    for file in (submission_recur, submission_invalid, submission_wrong):
+        print(compare_submission_solution(starter, file, solution))
