02322a9865f6857b69a91c789d4154184d2cd07e
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2022-01-15 15:05:57 -0800

    Remove duplicate examples

--- comparisons/examples/fibonacci_recur_no_doc.py
+++
@@ -1,2 +0,0 @@
-def fm(n):
-    return fm(n-1)+fm(n-2) if n > 1 else n
--- comparisons/examples/wrong_no_doc.py
+++
@@ -1,7 +0,0 @@
-def fib(n):
-    prev = 0
-    curr = 1
-    for i in range(n - 1):
-        curr = curr + prev
-        prev = curr
-    return curr
