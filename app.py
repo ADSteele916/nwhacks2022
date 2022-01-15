4befbb2ca80ad264ac9e8577610fe9d2e9e2b7ea
Renu-R <aerorenu@gmail.com>
2022-01-15 13:57:22 -0800

    flask reads files and returns text as string

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+app.config["UPLOAD_FOLDER"] = "static/"
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def upload_file():
+    return render_template('dummy.html')
 
+
+@app.route('/display', methods = ['GET', 'POST'])
+def display_file():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        f1 = request.files['file1']
+        f2 = request.files['file2']
+        filename1 = secure_filename(f1.filename)
+        filename2 = secure_filename(f2.filename)
+
+        f1.save(app.config['UPLOAD_FOLDER'] + filename1)
+
+        file = open(app.config['UPLOAD_FOLDER'] + filename1,"r")
+        content = file.read()   
+        
+    return render_template('display.html', content=type(content)) 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug = True)
