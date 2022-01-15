021308aee62d26f52e50d6e68803ce03f9d7becd
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2022-01-15 15:05:07 -0800

    Update comparisons

--- comparisons/comparison_funcs.py
+++ comparisons/comparison_funcs.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import Tuple
 
 import openai
 
@@ -10,7 +11,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def compare_submission_solution(
     starter_file: Path, submission_file: Path, solution_file: Path
-):
+) -> Tuple[float, float, float]:
     starter, submission, solution = (
         file_path_to_code_string(starter_file),
         file_path_to_code_string(submission_file),
@@ -22,16 +23,36 @@ def compare_submission_solution(
     out = engine.search(
         search_model="davinci-codex", documents=[starter, solution], query=submission
     )
-    return out["data"][0]["score"], out["data"][1]["score"]
+
+    starter_similarity = out["data"][0]["score"]
+    solution_similarity = out["data"][1]["score"]
+
+    return (
+        starter_similarity,
+        solution_similarity,
+        solution_similarity - starter_similarity,
+    )
 
 
 if __name__ == "__main__":
     starter = Path("examples/starter.py")
+    submission_random = Path("examples/submission_random_garbage.py")
     submission_wrong = Path("examples/submission_wrong_function.py")
     submission_recur = Path("examples/submission_recur.py")
     submission_invalid = Path("examples/submission_invalid.py")
     submission_indent_error = Path("examples/submission_indent_error.py")
     solution = Path("examples/solution.py")
 
-    for file in (submission_wrong, submission_recur, submission_invalid, submission_indent_error):
-        print(compare_submission_solution(starter, file, solution))
+    for file in (
+        starter,
+        submission_random,
+        submission_wrong,
+        submission_recur,
+        submission_invalid,
+        submission_indent_error,
+        solution,
+    ):
+        starter_similarity, solution_similarity, score = compare_submission_solution(
+            starter, file, solution
+        )
+        print(f"{starter_similarity}, {solution_similarity}; Score = {score}")
--- comparisons/file_parser.py
+++ comparisons/file_parser.py
@@ -5,7 +5,7 @@ from typing import List
 docstring = re.compile(r"(\"\"\"|''')(?:(?=(\\?))\2(.|\n))*?\1")
 
 
-def strip_comments(text: List[str]) -> str:
+def __strip_comments(text: List[str]) -> str:
     out = ""
     for line in text:
         no_comments = line.split("#")[0]
@@ -15,5 +15,5 @@ def strip_comments(text: List[str]) -> str:
 
 def file_path_to_code_string(file: Path) -> str:
     with open(file, 'r') as f:
-        text = strip_comments(f.readlines())
+        text = __strip_comments(f.readlines())
     return re.sub(docstring, "", text)
