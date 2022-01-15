55e74b6fa5ea0f8f5293d907018c3f9473be3baa
Tiffany Matthe <tiffanymatthe@gmail.com>
2022-01-15 12:29:17 -0800

    continued Similarity

--- comparisons/Similarity.py
+++ comparisons/Similarity.py
@@ -6,25 +6,28 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class Similarity:
     def __init__(self) -> None:
         self.engine = "davinci-codex"
+        summaries = {} # dictionary with keys = file ids and values = summaries
 
-    def get_summary(self, code: str) -> str:
+    def add_summary(self, code: str) -> str:
         prepend = "Python 3"
         append = "# Explanation of what the code does\n\n #"
         query = prepend + code + append
         response = openai.Completion.create(
-        engine=self.engine,
-        prompt=query,
-        temperature=0,
-        max_tokens=64,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=["#"]
+            engine=self.engine,
+            prompt=query,
+            temperature=0,
+            max_tokens=64,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["#"]
         )
+
+        
 
         return response
 
-    def get_similarity_score(self, summary_1: str, summary_2: str) -> int:
+    def get_similarity_score(self, problem_statement: str, summary_1: str, summary_2: str) -> int:
         openai.Engine("ada").search(
             search_model="ada", 
             query="happy", 
