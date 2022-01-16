435c3d9c9fbe63e0ad57ec60a6a405ca831d110f
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2022-01-16 10:50:11 -0800

    Apply black formatting

--- comparisons/Similarity.py
+++ comparisons/Similarity.py
@@ -1,25 +1,31 @@
 import os
-import openai
 from pathlib import Path
 
+import openai
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 class Similarity:
     def __init__(self) -> None:
         self.engine = "davinci-codex"
 
-    def get_similarity_score(self, problem_statement: str, summary_to_check: str, summary_reference: str) -> int:
+    def get_similarity_score(
+        self, problem_statement: str, summary_to_check: str, summary_reference: str
+    ) -> int:
         """Returns a positive similarity score usually between 0 and 300. Above 200 is good."""
         score_dict = openai.Engine(self.engine).search(
             search_model=self.engine,
-            query=problem_statement, 
+            query=problem_statement,
             max_rerank=2,
-            documents=[summary_to_check, summary_reference]
+            documents=[summary_to_check, summary_reference],
         )
 
         return score_dict
 
-    def compare_submission_solution(self, starter_file: Path, submission_file: Path, solution_file: Path):
+    def compare_submission_solution(
+        self, starter_file: Path, submission_file: Path, solution_file: Path
+    ):
         """Returns submission similarity score with starter_file and with solution_file."""
         with open(starter_file) as file:
             starter = file.read()
@@ -30,10 +36,10 @@ class Similarity:
 
         engine = openai.Engine(self.engine)
 
-        out = engine.search(search_model=self.engine, documents=[starter, solution], query=submission)
+        out = engine.search(
+            search_model=self.engine, documents=[starter, solution], query=submission
+        )
 
         starter_result = out["data"][0]
         solution_result = out["data"][1]
         return starter_result["score"], solution_result["score"]
-
-
