98186f0cb68c2541d9b288d6c9de6d3bd4bc069c
Tiffany Matthe <tiffanymatthe@gmail.com>
2022-01-15 22:44:43 -0800

    add code formatter

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.file-upload').change(function() {
         var file = $(this)[0].files[0].name;
         $(this).next('p').text(file);
-        pre_child = $(this).siblings('pre');
+        pre_child = $(this).siblings('pre').children();
         var fileReader = new FileReader();
         fileReader.onload = function(fileLoadedEvent){
             var textFromFileLoaded = fileLoadedEvent.target.result;
