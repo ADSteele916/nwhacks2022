01160f9542cda14cc7a31a28e92da39106988050
Renu-R <aerorenu@gmail.com>
2022-01-15 19:16:31 -0800

    HTTP requests from both portals work

--- app.py
+++ app.py
@@ -21,35 +21,41 @@ def teacher_page():
     return render_template('teacher.html')
 
 
-@app.route('/display', methods = ['GET', 'POST'])
+@app.route('/teacher_results', methods = ['GET', 'POST'])
 def display_file():
     if request.method == 'POST':
+        starter = request.files['starter']
+        f1 = request.files['file1']
+        f2 = request.files['file2']
+        starter_name = secure_filename(starter.filename)
+        filename1 = secure_filename(f1.filename)
+        filename2 = secure_filename(f2.filename)
 
-        if request.files['starter'] is not None:
+        starter.save(app.config['UPLOAD_FOLDER'] + starter_name)
+        f1.save(app.config['UPLOAD_FOLDER'] + filename1)
+        f2.save(app.config['UPLOAD_FOLDER'] + filename2)
 
-            starter = request.files['starter']
-            f1 = request.files['file1']
-            f2 = request.files['file2']
-            starter_name = secure_filename(starter.filename)
-            filename1 = secure_filename(f1.filename)
-            filename2 = secure_filename(f2.filename)
+        starter_file = open(app.config['UPLOAD_FOLDER'] + starter_name,"r")
+        file1 = open(app.config['UPLOAD_FOLDER'] + filename1,"r")
+        file2 = open(app.config['UPLOAD_FOLDER'] + filename2,"r")
 
-            starter.save(app.config['UPLOAD_FOLDER'] + starter_name)
-            f1.save(app.config['UPLOAD_FOLDER'] + filename1)
-            f2.save(app.config['UPLOAD_FOLDER'] + filename2)
-
-            starter_file = open(app.config['UPLOAD_FOLDER'] + starter_name,"r")
-            file1 = open(app.config['UPLOAD_FOLDER'] + filename1,"r")
-            file2 = open(app.config['UPLOAD_FOLDER'] + filename2,"r")
-
-            starter_text = file_text_to_code_string(starter_file)
-            submission = file_text_to_code_string(file1)
-            solution = file_text_to_code_string(file2)
-            results = is_passing(compare_submission_solution(starter_text, submission, solution))
-
-        
-        
+        starter_text = file_text_to_code_string(starter_file)
+        submission = file_text_to_code_string(file1)
+        solution = file_text_to_code_string(file2)
+        results = is_passing(compare_submission_solution(starter_text, submission, solution))
     return render_template('results.html', content=results) 
+
+@app.route('/student_results', methods = ['GET', 'POST'])
+def student_stuff():
+    if request.files['student_file']:
+        code = request.files['student_file']
+        code_name = secure_filename(code.filename)
+        code.save(app.config['UPLOAD_FOLDER'] + code_name)
+        code_file = open(app.config['UPLOAD_FOLDER'] + code_name,"r")
+        code_text = file_text_to_code_string(code_file)
+    return render_template('results.html', content=code_text) 
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug = True)
