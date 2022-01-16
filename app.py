8cd6a5edb29f94f921dd4fd2960a1c88627fb2f6
Renu-R <aerorenu@gmail.com>
2022-01-15 18:16:06 -0800

    teacher portal working

--- app.py
+++ app.py
@@ -24,25 +24,30 @@ def teacher_page():
 @app.route('/display', methods = ['GET', 'POST'])
 def display_file():
     if request.method == 'POST':
-        starter = request.files['starter']
-        f1 = request.files['file1']
-        f2 = request.files['file2']
-        starter_name = secure_filename(starter.filename)
-        filename1 = secure_filename(f1.filename)
-        filename2 = secure_filename(f2.filename)
 
-        starter.save(app.config['UPLOAD_FOLDER'] + starter_name)
-        f1.save(app.config['UPLOAD_FOLDER'] + filename1)
-        f2.save(app.config['UPLOAD_FOLDER'] + filename2)
+        if request.files['starter'] is not None:
 
-        starter_file = open(app.config['UPLOAD_FOLDER'] + starter_name,"r")
-        file1 = open(app.config['UPLOAD_FOLDER'] + filename1,"r")
-        file2 = open(app.config['UPLOAD_FOLDER'] + filename2,"r")
+            starter = request.files['starter']
+            f1 = request.files['file1']
+            f2 = request.files['file2']
+            starter_name = secure_filename(starter.filename)
+            filename1 = secure_filename(f1.filename)
+            filename2 = secure_filename(f2.filename)
 
-        starter_text = file_text_to_code_string(starter_file)
-        submission = file_text_to_code_string(file1)
-        solution = file_text_to_code_string(file2)
-        results = compare_submission_solution(starter_text, submission, solution)
+            starter.save(app.config['UPLOAD_FOLDER'] + starter_name)
+            f1.save(app.config['UPLOAD_FOLDER'] + filename1)
+            f2.save(app.config['UPLOAD_FOLDER'] + filename2)
+
+            starter_file = open(app.config['UPLOAD_FOLDER'] + starter_name,"r")
+            file1 = open(app.config['UPLOAD_FOLDER'] + filename1,"r")
+            file2 = open(app.config['UPLOAD_FOLDER'] + filename2,"r")
+
+            starter_text = file_text_to_code_string(starter_file)
+            submission = file_text_to_code_string(file1)
+            solution = file_text_to_code_string(file2)
+            results = is_passing(compare_submission_solution(starter_text, submission, solution))
+
+        
         
     return render_template('results.html', content=results) 
 
