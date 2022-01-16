17a43506bd88eb993d27a6d5e16678f35b25dbd3
Tiffany Matthe <tiffanymatthe@gmail.com>
2022-01-16 08:36:35 -0800

    styled results

--- static/js/main.js
+++ static/js/main.js
@@ -7,10 +7,14 @@ $(document).ready(function(){
         fileReader.onload = function(fileLoadedEvent){
             var textFromFileLoaded = fileLoadedEvent.target.result;
             pre_child.text(textFromFileLoaded);
-            hljs.initHighlightingOnLoad();
             hljs.highlightAll();
         };
 
         fileReader.readAsText($(this)[0].files[0], "UTF-8");
     });
+
+    $(window).bind("load", function() { 
+        console.log("here")
+        hljs.highlightAll();
+     });
 });
